4fd13c67f60e8cfd05b42bbc4f5795630e588de8
botelhogustavo <gustavodasilvabotelho@gmail.com>
2025-05-22 11:47:22 -0300

    Update ex05.cpp

--- ex05.cpp
+++ ex05.cpp
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <stdio.h>
-
 void inverter(int a[], int t) {
     int *inicio = a;        // aponta para o primeiro elemento
     int *fim = a + t - 1;   // aponta para o ultimo elemento
